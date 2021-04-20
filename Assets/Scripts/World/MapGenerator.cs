fde4a94a6ec4acb21676be8904127cd6acad2301
StoneLabs <levyehrstein@googlemail.com>
2021-04-20 23:46:38 +0200

    Add Perlin Noise generator

---
+++ Assets/Scripts/World/MapGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MapDisplay))]
+public class MapGenerator : MonoBehaviour
+{
+    public int mapWidth = 0;
+    public int mapHeight = 0;
+
+    [Range(0.0001f, 20)]
+    public float noiseScale = 0;
+
+    // This is used in the Editor extension
+    public bool autoUpdate;
+
+    public void GenerateMap()
+    {
+        var map = NoiseGenerator.GenerateNoisemap(mapWidth, mapHeight, noiseScale);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(map);
+    }
+}
---
+++ Assets/Scripts/World/NoiseGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TreeEditor;
+using UnityEngine;
+
+public static class NoiseGenerator
+{
+    public static float[,] GenerateNoisemap(int width, int height, float scale)
+    {
+        if (width <= 0 || height <= 0)
+            throw new ArgumentException("Illegal dimensions supplied to Noisemap generator");
+
+        if (scale <= 0)
+            throw new ArgumentException("Illegal scale supplied to Noisemap generator");
+
+        // 2d float array to store perlin noise data
+        float[,] noiseMap = new float[width, height];
+
+        for (int y = 0; y < height; y++)
+            for (int x = 0; x < width; x ++)
+                noiseMap[x, y] = Mathf.PerlinNoise(x / scale, y / scale);
+
+        return noiseMap;
+    }
+}
