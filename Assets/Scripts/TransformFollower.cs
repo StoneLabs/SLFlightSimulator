98ee7e0fdb6abb41175a85e15c1037114e508a8e
StoneLabs <levyehrstein@googlemail.com>
2021-04-26 18:49:52 +0200

    Add Clouds and skybox

---
+++ Assets/Scripts/TransformFollower.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformFollower : MonoBehaviour
+{
+    public Transform reference;
+    public bool followX;
+    public bool followY;
+    public bool followZ;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = this.transform.position - reference.position;
+    }
+
+    void Update()
+    {
+        if (followX)
+            this.transform.position = new Vector3(reference.position.x + offset.x, this.transform.position.y, this.transform.position.z);
+        if (followY)
+            this.transform.position = new Vector3(this.transform.position.x, reference.position.y + offset.y, this.transform.position.z);
+        if (followZ)
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, reference.position.z + offset.z);
+    }
+}
