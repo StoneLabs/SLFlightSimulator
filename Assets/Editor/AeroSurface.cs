fe6fee7926d55eaa441cd028e5b82c893f7e0ca6
StoneLabs <levyehrstein@googlemail.com>
2021-05-19 22:41:53 +0200

    Add Aero Surface component

---
+++ Assets/Editor/AeroSurface.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AeroSurface))]
+public class AeroSurfaceEditor : Editor
+{
+    public bool autoUpdate = true;
+    float area;
+
+    private void calculatePreview(AeroSurface surface)
+    {
+        this.area = surface.SurfaceArea;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        AeroSurface surface = (AeroSurface)target;
+        bool defaultChange = DrawDefaultInspector();
+
+        GUILayout.Space(10);
+        EditorGUILayout.LabelField("Editor preview", EditorStyles.boldLabel);
+
+        EditorGUILayout.LabelField($"Area:\t{area}");
+
+        if (autoUpdate)
+            calculatePreview(surface);
+
+        GUILayout.Space(10);
+        autoUpdate = GUILayout.Toggle(autoUpdate, "Auto-update");
+        if (GUILayout.Button("Calculate"))
+            calculatePreview(surface);
+    }
+}
---
+++ Assets/Scripts/Plane/AeroSurface.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AeroSurface : MonoBehaviour
+{
+    [Range(0, 3)]
+    public float width = 0;
+    [Range(0, 3)]
+    public float height = 0;
+
+    public float SurfaceArea
+    {
+        get
+        {
+            return transform.lossyScale.x * width * transform.lossyScale.z * height;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnDrawGizmos()
+    {
+#if UNITY_EDITOR
+        Gizmos.color = Color.red;
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawCube(Vector3.zero, new Vector3(width, 0.001f, height));
+#endif
+    }
+}
