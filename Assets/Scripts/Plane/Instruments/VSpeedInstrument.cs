932e82b6e68d17393138dffac05cfe03da194c58
StoneLabs <levyehrstein@googlemail.com>
2021-05-25 16:32:47 +0200

    Fix VSpeed Instrument

--- Assets/Scripts/Plane/Instruments/VSpeedInstrument.cs
+++ Assets/Scripts/Plane/Instruments/VSpeedInstrument.cs
@@ -7,9 +7,14 @@ public class VSpeedInstrument : Instrument
     public Transform needle;
     [Range(-90, 90)]
     public float dregreePerFeet = 9;
+    public bool clamp = true;
 
     void Update()
     {
-        needle.localRotation = Quaternion.Euler(-90, manager.physics.body.velocity.y, 0);
+        float hundretFeetPerMinute = UnitConverter.Meter2Feet(manager.physics.body.velocity.y) / 100 * 60;
+        if (clamp)
+            needle.localRotation = Quaternion.Euler(-90, Mathf.Clamp(hundretFeetPerMinute * dregreePerFeet, -175, 175), 0);
+        else
+            needle.localRotation = Quaternion.Euler(-90, hundretFeetPerMinute * dregreePerFeet, 0);
     }
 }
