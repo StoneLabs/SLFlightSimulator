1c4d9639d3649d27e1d5ff3c188347164562ca67
StoneLabs <levyehrstein@googlemail.com>
2021-05-25 18:26:55 +0200

    Add Virtual Horizon Instrument

---
+++ Assets/Scripts/Plane/Instruments/RPYInstrument.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RPYInstrument : Instrument
+{
+    public Transform ball;
+    [Range(0, 100)]
+    public float speed = 10.0f;
+    public Vector3 installCorrection;
+
+    void Update()
+    {
+        Vector3 target = new Vector3(90, manager.physics.body.rotation.eulerAngles.y, 0) + installCorrection;
+        ball.rotation = Quaternion.Slerp(ball.rotation, Quaternion.Euler(target), speed * Time.deltaTime);
+    }
+}
