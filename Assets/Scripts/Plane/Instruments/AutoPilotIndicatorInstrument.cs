26f4c7ec4fdb372d003cd09c81a14860ac921597
StoneLabs <levyehrstein@googlemail.com>
2021-05-25 19:47:50 +0200

    Add AutoPilot indicator

---
+++ Assets/Scripts/Plane/Instruments/AutoPilotIndicatorInstrument.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoPilotIndicatorInstrument : Instrument
+{
+    public MeshRenderer indicator;
+    public Material onMaterial;
+    public Material offMaterial;
+
+    private void Update()
+    {
+        if (manager.IsAutoPilot())
+            indicator.material = onMaterial;
+        else
+            indicator.material = offMaterial;
+    }
+}
