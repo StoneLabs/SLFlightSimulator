5e8ec67eeb0cc1601590b42cabc49d993b336a1b
StoneLabs <levyehrstein@googlemail.com>
2021-05-25 18:35:42 +0200

    Fix contrast of debug information

--- Assets/Scripts/Plane/PlaneManager.cs
+++ Assets/Scripts/Plane/PlaneManager.cs
@@ -61,18 +61,22 @@ public class PlaneManager : MonoBehaviour
         if (!drawDebug)
             return;
 
+        GUI.Box(new Rect(0, 150, 310, 340), "");
         int y = 150;
-        GUI.Label(new Rect(0, y, 400, 400), $"Velocity: {GetComponent<Rigidbody>().velocity.magnitude * 3.6f:F2}km/h");
-        GUI.Label(new Rect(0, y += 20, 400, 400), $"Altitude: {transform.position.y:F2}m ASL");
-        GUI.Label(new Rect(0, y += 20, 400, 400), $"Plane Mass: {physics.body.mass:F2}kg ({physics.DryMass:F2}kg Dry)");
-        GUI.Label(new Rect(0, y += 20, 400, 400), $"Fuel level: {fuelLevel:F1}L / {fuelCapacity:F1}L ({FuelPercentage*100.0f:F2}%)");
-        GUI.Label(new Rect(0, y += 40, 400, 400), $"Temperature: {environment.CalculateTemperature(transform.position.y) - 273.15f:F2}C");
-        GUI.Label(new Rect(0, y += 20, 400, 400), $"Pressure: {environment.CalculatePressure(transform.position.y):F2}pascal");
-        GUI.Label(new Rect(0, y += 20, 400, 400), $"Density: {environment.CalculateDensity(transform.position.y):F2}kg/m3");
-        GUI.HorizontalSlider(new Rect(0, y += 40, 200, 40), throttle, 0, 1);
-        GUI.HorizontalSlider(new Rect(0, y += 20, 200, 40), steeringPitch, -1, 1);
-        GUI.HorizontalSlider(new Rect(0, y += 20, 200, 40), steeringRoll, -1, 1);
-        GUI.HorizontalSlider(new Rect(0, y += 20, 200, 40), steeringYaw, -1, 1);
+        GUI.Label(new Rect(5, y, 300, 400), "PLANE DEBUG INFORMATION");
+        GUI.Label(new Rect(5, y += 40, 300, 400), $"World Position ({transform.position.x / 1000:F2}, {transform.position.z / 1000:F2})km");
+        GUI.Label(new Rect(5, y += 20, 300, 400), $"Velocity: {GetComponent<Rigidbody>().velocity.magnitude * 3.6f:F2}km/h");
+        GUI.Label(new Rect(5, y += 20, 300, 400), $"Altitude: {transform.position.y:F2}m ASL");
+        GUI.Label(new Rect(5, y += 20, 300, 400), $"Plane Mass: {physics.body.mass:F2}kg ({physics.DryMass:F2}kg Dry)");
+        GUI.Label(new Rect(5, y += 20, 300, 400), $"Fuel level: {fuelLevel:F1}L / {fuelCapacity:F1}L ({FuelPercentage*100.0f:F2}%)");
+        GUI.Label(new Rect(5, y += 40, 300, 400), $"Temperature: {environment.CalculateTemperature(transform.position.y) - 273.15f:F2}C");
+        GUI.Label(new Rect(5, y += 20, 300, 400), $"Pressure: {environment.CalculatePressure(transform.position.y):F2}pascal");
+        GUI.Label(new Rect(5, y += 20, 300, 400), $"Density: {environment.CalculateDensity(transform.position.y):F2}kg/m3");
+        GUI.Label(new Rect(5, y += 40, 300, 400), $"Throttle, Pitch, Roll, Yaw:");
+        GUI.HorizontalSlider(new Rect(5, y += 20, 300, 40), throttle, 0, 1);
+        GUI.HorizontalSlider(new Rect(5, y += 20, 300, 40), steeringPitch, -1, 1);
+        GUI.HorizontalSlider(new Rect(5, y += 20, 300, 40), steeringRoll, -1, 1);
+        GUI.HorizontalSlider(new Rect(5, y += 20, 300, 40), steeringYaw, -1, 1);
     }
 
     public float getSteeringRoll()
