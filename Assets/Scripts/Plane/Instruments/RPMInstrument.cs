70e090861b610287e5dff6af4a61576ad675e1a4
StoneLabs <levyehrstein@googlemail.com>
2021-05-31 22:30:43 +0200

    Add RPM instrument

---
+++ Assets/Scripts/Plane/Instruments/RPMInstrument.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RPMInstrument : Instrument
+{
+    public int engine;
+    public Transform needle;
+    [Range(-90, 90)]
+    public float dregreePerFeet = 9f;
+
+    void Update()
+    {
+        needle.localRotation = Quaternion.Euler(90 + manager.physics.engines[engine].RPM / 100.0f * dregreePerFeet, -90, -90);
+    }
+}
