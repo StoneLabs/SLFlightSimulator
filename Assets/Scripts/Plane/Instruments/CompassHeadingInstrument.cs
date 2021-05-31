7c412622a2b29fe75fdb93c51f1d29f72a01596e
StoneLabs <levyehrstein@googlemail.com>
2021-05-31 23:01:19 +0200

    Add Compass Instrument

---
+++ Assets/Scripts/Plane/Instruments/CompassHeadingInstrument.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompassHeadingInstrument : Instrument
+{
+    public Transform cylinder;
+    [Range(-90, 90)]
+    public float degreeOffset = 45f;
+    [Range(-90, 90)]
+    public float degreeperDegree = -1f;
+
+    void Update()
+    {
+        cylinder.localRotation = Quaternion.Euler(90 + manager.physics.Heading * degreeperDegree + degreeOffset, -90, -90);
+    }
+}
