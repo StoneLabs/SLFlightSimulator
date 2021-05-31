b7c08039a245a3ffea27e4b0af18b9687a74653c
StoneLabs <levyehrstein@googlemail.com>
2021-05-31 22:44:46 +0200

    Add Heading Instrument

---
+++ Assets/Scripts/Plane/Instruments/HeadingInstruments.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadingInstruments : Instrument
+{
+    public Transform needle;
+    [Range(-90, 90)]
+    public float degreeOffset = 45f;
+    [Range(-90, 90)]
+    public float degreeperDegree = -1f;
+
+    void Update()
+    {
+        needle.localRotation = Quaternion.Euler(90 + manager.physics.Heading * degreeperDegree + degreeOffset, -90, -90);
+    }
+}
