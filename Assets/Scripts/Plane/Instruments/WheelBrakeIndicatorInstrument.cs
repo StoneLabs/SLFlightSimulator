472c16550196303450e7ef0ebbf7a00008868583
StoneLabs <levyehrstein@googlemail.com>
2021-06-07 18:05:52 +0200

    Add WheelBrake Indicator Instrument

---
+++ Assets/Scripts/Plane/Instruments/WheelBrakeIndicatorInstrument.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelBrakeIndicatorInstrument : Instrument
+{
+    public MeshRenderer indicator;
+    public Material onMaterial;
+    public Material offMaterial;
+
+    private void Update()
+    {
+        if (manager.WheelBreaks)
+            indicator.material = onMaterial;
+        else
+            indicator.material = offMaterial;
+    }
+}
