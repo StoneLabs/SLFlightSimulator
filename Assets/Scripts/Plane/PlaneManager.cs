9f531d278aa8b3f9e625494eb4afc3e7bbfa9556
StoneLabs <levyehrstein@googlemail.com>
2021-06-08 21:36:10 +0200

    Fix keyhelp containing obsolete key

--- Assets/Scripts/Plane/PlaneManager.cs
+++ Assets/Scripts/Plane/PlaneManager.cs
@@ -164,14 +164,13 @@ public class PlaneManager : MonoBehaviour
         GUI.Label(new Rect(5, y += 20, 300, 400), WheelBreaks ? $"Wheel Brakes engaged!" : "");
         GUI.Label(new Rect(5, y += 40, 300, 400), $"Engine 1 RPM: {physics.engines[0].RPM:F0}");
 
-        GUI.Box(new Rect(Screen.width - 185, 150, 185, 285 + respawnPoints.Length * 20), "");
+        GUI.Box(new Rect(Screen.width - 185, 150, 185, 265 + respawnPoints.Length * 20), "");
         GUI.Label(new Rect(Screen.width - 180, y = 150, 300, 400), $"CONTROLS");
         GUI.Label(new Rect(Screen.width - 180, y += 40, 300, 400), $"Shift/Ctrl - Throttle");
         GUI.Label(new Rect(Screen.width - 180, y += 20, 300, 400), $"W/S - Pitch");
         GUI.Label(new Rect(Screen.width - 180, y += 20, 300, 400), $"A/D - Roll");
         GUI.Label(new Rect(Screen.width - 180, y += 20, 300, 400), $"Q/E - Yaw");
         GUI.Label(new Rect(Screen.width - 180, y += 40, 300, 400), $"T - Toggle Autopilot");
-        GUI.Label(new Rect(Screen.width - 180, y += 20, 300, 400), $"R - Reset plane");
         GUI.Label(new Rect(Screen.width - 180, y += 20, 300, 400), $"O - Toggle wind");
         GUI.Label(new Rect(Screen.width - 180, y += 20, 300, 400), $"L - Toggle Aerobatic smoke");
         GUI.Label(new Rect(Screen.width - 180, y += 20, 300, 400), $"M - Toggle audio");
