fbd68b8502724f0170a55819f18d6507a388b78e
StoneLabs <levyehrstein@googlemail.com>
2021-11-09 13:06:48 +0100

    Add flaps indicator

---
+++ Assets/Scripts/Plane/Instruments/FlapsIndicatorInstrument.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Wheel brake indicator instrument. Changes material based on wheel brake status
+/// </summary>
+public class FlapsIndicatorInstrument : Instrument
+{
+    public MeshRenderer indicator;
+    public Material onMaterial;
+    public Material offMaterial;
+
+    private void Update()
+    {
+        // Change material
+        if (manager.SteeringFlaps)
+            indicator.material = onMaterial;
+        else
+            indicator.material = offMaterial;
+    }
+}
